7cd29b04c33497895792f7205d2bb88b85e47f12
Bhumika76-web <bhumikarajput0005@gmail.com>
2026-02-12 15:23:38 +0530

    Security Updated

--- src/main/java/com/signupproject/signup/SignupApplication.java
+++ src/main/java/com/signupproject/signup/SignupApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SignupApplication {
+public class
+SignupApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SignupApplication.class, args);
